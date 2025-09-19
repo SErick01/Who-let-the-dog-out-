e676cb3523b742d90c2d73be2a34cd51b5764967
Ellie Brinkman <brinkmanellie12@gmail.com>
2025-09-19 01:09:18 -0400

    Scripts & Controllers
    changed movement for dog, added sample lane with cars that "respawn" -- no collison detction yet :(

---
+++ Assets/Movement2.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    public float moveDistance = 1.25f;
+    void Update()
+    {
+        //moves up and down
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            Move(Vector2.up);
+            if (animator != null)
+            {
+                animator.SetTrigger("MoveTrigger"); // Ensure this trigger exists in your Animator
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+
+            Move(Vector2.down);
+            if (animator != null)
+            {
+                animator.SetTrigger("MoveTrigger"); // Ensure this trigger exists in your Animator
+            }
+        }
+        else
+        {
+            animator.ResetTrigger("MoveTrigger");
+        }
+    }
+
+    void Move(Vector2 direction)
+    {
+    transform.position += (Vector3)(direction * moveDistance);
+    }
+}
