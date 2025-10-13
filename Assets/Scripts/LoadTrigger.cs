7eaeb792854fd83ce1d4ba6643d96753a84e179f
Samantha <serick01@rams.shepherd.edu>
2025-10-13 14:15:18 -0400

    skip function added (spacebar, return, & mousepad enter)
    skip function added (spacebar, return, & mousepad enter)

--- Assets/Scripts/LoadTrigger.cs
+++ Assets/Scripts/LoadTrigger.cs
@@ -3,26 +3,37 @@ using UnityEngine;
 public class LoadTrigger : MonoBehaviour
 {
     public bool loadsNextScene;
-
     public int sceneIndexToLoad;
-
     public float WaitTime = 3f;
+    public bool skipped;
 
-
-    void Start()
-    {
-        Load();
-    }
+    void Start() {Load();}
 
     public void Load()
     {
         if (loadsNextScene)
-        {
-            LoadScene.Instance.WaitToLoadNextScene(WaitTime);
-        }
+        {LoadScene.Instance.WaitToLoadNextScene(WaitTime);}
+        
         else
+        {LoadScene.Instance.WaitToLoadIndex(WaitTime, sceneIndexToLoad);}
+    }
+
+    void Update()
+    {
+        if (skipped) {return;}
+
+        if (Input.GetKeyDown(KeyCode.Space) ||
+            Input.GetKeyDown(KeyCode.Return) ||
+            Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            LoadScene.Instance.WaitToLoadIndex(WaitTime, sceneIndexToLoad);
+            skipped = true;
+            LoadScene.Instance.StopAllCoroutines();
+
+            if (loadsNextScene)
+            {LoadScene.Instance.LoadNextScene();}
+            
+            else
+            {LoadScene.Instance.LoadSceneByIndex(sceneIndexToLoad);}
         }
     }
 }
